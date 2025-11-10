b5bdbf39914be7eea56b44f6ef2f0cdf144b7e8f
kubiknem <jaknem.lbc@gmail.com>
2025-11-10 02:04:39 +0100

    Merged newly modified utils files to old ones

--- hybrid-clingo/utils/make_readable_v2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# adds new lines to the output of clingo-lpx to improve readability
-
-sed -E 's|) |)\n|g; s|([0-9]) |\1\n|g; s|([-+*])e |\1e\n|g; s|=e |=e\n|g'
--- hybrid-clingo/utils/replace_assignments.sh
+++ hybrid-clingo/utils/replace_assignments.sh
@@ -1,21 +1,39 @@
-#\bin\bash
+#!/bin/bash
 
 # takes replaces terms/atoms in the Answer by their corresponding interpreted values from the Assignment
-# TODO currently works only when there is a single answer
 
-output=$(cat)
+MODE="Skip"
+ANSW_HEAD=""
 
-answer=$(echo "$output" | grep -A1 "Answer:" | grep -v "Answer:")
-answer_AsLines=$(echo "$answer" | sed 's|) |)\n|g')
-assignment_AsLines=$(echo "$output" | grep -A1 "Assignment:" | grep -v "Assignment:" | sed 's|\([0-9]\) |\1\n|g')
+while IFS= read LINE
+    do
+        case "$LINE" in
+            Answer:*)
+                echo "$LINE"
+                MODE="Answer"
+                ;;
+            Assignment:*)
+                MODE="Assignment"
+                ;;
+            *)
+                if [ $MODE = "Answer" ]; then
+                    answer_AsLines=$(echo "$LINE" | sed 's|) |)\n|g')
+                    MODE="Skip"
+                elif [ $MODE = "Assignment" ]; then
+                    assignment_AsLines=$(echo "$LINE" | sed 's|\([0-9]\) |\1\n|g')
+                    newAnswer_AsLines=$(echo "$answer_AsLines")
+                    for assignment in $(echo "$assignment_AsLines"); do
+                        name=$(echo "$assignment" | cut -d = -f 1)
+                        value=$(echo "$assignment" | cut -d = -f 2)
+                        newAnswer_AsLines=$(echo "$newAnswer_AsLines" | sed "s|$name|$value|")
+                    done
+                    newAnswer=$(echo $newAnswer_AsLines)
+                    echo "$newAnswer"
+                    MODE="Skip"
+                else
+                    echo "$LINE"
+                fi
+                ;;
+        esac
 
-newAnswer_AsLines=$(echo "$answer_AsLines")
-for assignment in $(echo "$assignment_AsLines"); do
-    name=$(echo "$assignment" | cut -d = -f 1)
-    value=$(echo "$assignment" | cut -d = -f 2)
-    newAnswer_AsLines=$(echo "$newAnswer_AsLines" | sed "s|$name|$value|")
-done
-
-newAnswer=$(echo $newAnswer_AsLines)
-
-echo "$output" | sed "s|$answer|$newAnswer|" | grep -v "Assignment:\|^timeAtStep"
+	done
