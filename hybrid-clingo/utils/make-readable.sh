4fb2262cff98e2a928996e32762864566d02567e
kubiknem <jaknem.lbc@gmail.com>
2025-11-10 02:01:10 +0100

    Merged newly modified utils files to old ones

--- hybrid-clingo/utils/make-readable.sh
+++ hybrid-clingo/utils/make-readable.sh
@@ -1,5 +1,5 @@
-#\bin\bash
+#!/bin/bash
 
 # adds new lines to the output of clingo-lpx to improve readability
 
-cat | sed 's|) |)\n|g' | sed 's|\([0-9]\) |\1\n|g' | sed 's|\([-+*]\)e |\1e\n|g' | sed 's|=e |=e\n|g'
+sed -E 's|) |)\n|g; s|([0-9]) |\1\n|g; s|([-+*])e |\1e\n|g; s|=e |=e\n|g'
